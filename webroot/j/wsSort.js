0bb4a0610952b0b8d3f83e58dac5f7e7fd45eaf7
Alex Ford <alex@ford-web.net>
2024-08-10 04:35:43 -0500

    Can now sort the statically-defined list and get results.

--- webroot/j/wsSort.js
+++ webroot/j/wsSort.js
@@ -26,10 +26,31 @@ window.addEventListener("DOMContentLoaded", () => {
                 document.getElementById("b").textContent = mesg.b;
                 break;
             case "result":
+                document.getElementById("rank").style.display = "none";
+                document.getElementById("result").style.display = "block";
+                const result_list = document.getElementById("result_list");
+                for (const item_text of mesg.data) {
+                    const list_item = document.createElement("li");
+                    list_item.textContent = item_text;
+                    result_list.appendChild(list_item);
+                };
                 break;
             default:
                 console.error("unsupported event type", mesg);
         };
+    };
 
-    }
+    document.getElementById("a").onclick = () => {
+        websocket.send(JSON.stringify({
+            "type": "answer",
+            "answer": "a"
+        }));
+    };
+
+    document.getElementById("b").onclick = () => {
+        websocket.send(JSON.stringify({
+            "type": "answer",
+            "answer": "b"
+        }));
+    };
 })
