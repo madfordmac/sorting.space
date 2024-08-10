d8a1c94e7c5cf91a6d04ba2be2365c574cccc494
Alex Ford <alex@ford-web.net>
2024-08-10 02:18:53 -0500

    Logging fixes.

--- sortd/__main__.py
+++ sortd/__main__.py
@@ -4,21 +4,29 @@ from . import sorter
 import asyncio
 import logging
 from logging.handlers import SysLogHandler
-import sys
+import sys, os
 
 async def main() -> None:
     # Set up logging
     logger = logging.getLogger('sspace')
     logger.setLevel(logging.DEBUG)
-    syslog = SysLogHandler(address='/dev/log', facility=SysLogHandler.LOG_LOCAL3)
-    syslog.setLevel(logging.INFO)
-    syslog.setFormatter(logging.Formatter('%(name)s[%(process)d]: %(message)s'))
-    logger.addHandler(syslog)
+    fwn_formatter = logging.Formatter('%(asctime)s : %(name)s[%(process)d] : %(levelname)s :: %(message)s')
+    if os.path.exists('/dev/log'):
+        syslog = SysLogHandler(address='/dev/log', facility=SysLogHandler.LOG_LOCAL3)
+        syslog.setLevel(logging.INFO)
+        syslog.setFormatter(logging.Formatter('%(name)s[%(process)d]: %(message)s'))
+        logger.addHandler(syslog)
+    else:
+        filelg = logging.StreamHandler(open('sortd.log', 'a'))
+        filelg.setLevel(logging.INFO)
+        filelg.setFormatter(fwn_formatter)
+        logger.addHandler(filelg)
     if sys.stdout.isatty():
         handlr = logging.StreamHandler()
         handlr.setLevel(logging.DEBUG)
-        handlr.setFormatter(logging.Formatter('%(asctime)s : %(name)s[%(process)d] : %(levelname)s :: %(message)s'))
+        handlr.setFormatter(fwn_formatter)
         logger.addHandler(handlr)
+    logger.info("👀 Logging started for sortd Websocket daemon.")
     
     # Serve the socket
     async with websockets.serve(sorter, 'localhost', 9832):
