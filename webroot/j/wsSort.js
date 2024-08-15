b43e41be8cb3e6309c77ea8e96bb241a8591dada
Alex Ford <alex@ford-web.net>
2024-08-14 22:42:51 -0500

    Point socket at domain name.

--- webroot/j/wsSort.js
+++ webroot/j/wsSort.js
@@ -1,6 +1,6 @@
 window.addEventListener("DOMContentLoaded", () => {
     // Open web socket
-    const websocket = new WebSocket("ws://localhost:9832/");
+    const websocket = new WebSocket("ws://sorting.space:9832/");
 
     // Debug action auto-submits a list
     /* websocket.onopen = () => websocket.send(JSON.stringify({
